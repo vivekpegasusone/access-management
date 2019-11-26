7aa12454166dc0e0ad634d60947c294ede95bc8f
Vivek Singh <viveksingh.pegasusone@gmail.com>
2019-11-26 21:53:52 +0530

    Code Refactoring.

--- src/main/java/com/drishti/accessmanagement/config/SecurityConfiguration.java
+++ src/main/java/com/drishti/accessmanagement/config/SecurityConfiguration.java
@@ -1,15 +1,28 @@
 package com.drishti.accessmanagement.config;
 
+import com.drishti.accessmanagement.handler.security.authentication.SuccessHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Set;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Autowired
+  private SuccessHandler successHandler;
 
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
@@ -21,24 +34,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         .loginProcessingUrl("/home")
         .usernameParameter("userId")
         .passwordParameter("password")
-        .successHandler(new AuthenticationSuccessHandler() {			
-          @Override
-          public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-            Authentication authentication) throws IOException, ServletException {
-            Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
-            if (roles.contains("ROLE_ADMIN")) {
-               response.sendRedirect("/access-management/admin");
-            } else {
-              response.sendRedirect("/access-management/user");
-            }
-          }
-        }).permitAll();
+        .successHandler(successHandler)
+        .permitAll();
   }
 
   @Autowired
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-  	  auth.inMemoryAuthentication()
-  	  .withUser("vivek").password("{noop}123").roles("ADMIN")
-  	  .and().withUser("amit").password("{noop}123").roles("USER");
-  } 
+    auth.inMemoryAuthentication()
+        .withUser("vivek").password("{noop}123").roles("ADMIN")
+        .and().withUser("amit").password("{noop}123").roles("USER");
+  }
 }
--- src/main/java/com/drishti/accessmanagement/controller/HomeController.java
+++ src/main/java/com/drishti/accessmanagement/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.drishti.accessmanagement.controller;
 
 import com.drishti.accessmanagement.dto.UserLogin;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
