ead2985a145c348992fda1206f7a0ed26d88c8ea
Vivek Singh <40670506+vivekpegasusone@users.noreply.github.com>
2019-12-02 18:20:55 +0530

    Update UserServiceImpl.java

--- src/main/java/com/drishti/accessmanagement/service/user/UserServiceImpl.java
+++ src/main/java/com/drishti/accessmanagement/service/user/UserServiceImpl.java
@@ -1,7 +1,9 @@
 package com.drishti.accessmanagement.service.user;
 
 import com.drishti.accessmanagement.dao.user.UserRepository;
+import com.drishti.accessmanagement.dto.role.RoleView;
 import com.drishti.accessmanagement.dto.user.UserView;
+import com.drishti.accessmanagement.entity.role.Role;
 import com.drishti.accessmanagement.entity.user.User;
 import com.drishti.accessmanagement.exception.RecordNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,80 +15,120 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static com.drishti.accessmanagement.utils.BeanUtility.copy;
-import static org.springframework.beans.BeanUtils.copyProperties;
-
 @Service
 @Transactional(readOnly = true)
 class UserServiceImpl implements UserService {
 
-  @Autowired
-  private UserRepository userRepository;
+	@Autowired
+	private UserRepository userRepository;
 
-  @Override
-  public List<UserView> getUsers() {
-    List<User> userList = userRepository.findByActiveTrue();
+	@Override
+	public List<UserView> getUsers() {
+		List<User> userList = userRepository.findByActiveTrue();
 
-    if (userList.size() > 0) {
-      return copy(userList, new ArrayList<>(), UserView.class);
-    } else {
-      return new ArrayList<>();
-    }
-  }
+		if (userList.size() > 0) {
+			return prepareUserViewsFromUsers(userList);
+		} else {
+			return new ArrayList<>();
+		}
+	}
 
-  @Override
-  public UserView findUserById(final Long id) throws RecordNotFoundException {
-    Optional<User> userOptional = userRepository.findById(id);
+	@Override
+	public UserView findUserById(final Long id) throws RecordNotFoundException {
+		Optional<User> userOptional = userRepository.findById(id);
 
-    if (userOptional.isPresent()) {
-      return prepareUserView(userOptional.get());
-    } else {
-      throw new RecordNotFoundException("No record exist for given user id " + id);
-    }
-  }
+		if (userOptional.isPresent()) {
+			return prepareUserView(userOptional.get());
+		} else {
+			throw new RecordNotFoundException("No record exist for given user id " + id);
+		}
+	}
 
-  @Override
-  public UserView findUserByLoginId(final String loginId) throws RecordNotFoundException {
-    Optional<User> userOptional = userRepository.findByLoginId(loginId);
+	@Override
+	public UserView findUserByLoginId(final String loginId) throws RecordNotFoundException {
+		Optional<User> userOptional = userRepository.findByLoginId(loginId);
 
-    if (userOptional.isPresent()) {
-      return prepareUserView(userOptional.get());
-    } else {
-      throw new RecordNotFoundException("No record exist for given user loginId " + loginId);
-    }
-  }
+		if (userOptional.isPresent()) {
+			return prepareUserView(userOptional.get());
+		} else {
+			throw new RecordNotFoundException("No record exist for given user loginId " + loginId);
+		}
+	}
 
-  @Override
-  @Transactional(propagation= Propagation.REQUIRED)
-  public UserView createUser(final UserView userView) {
-    return saveOrUpdateUser(userView);
-  }
+	@Override
+	@Transactional(propagation = Propagation.REQUIRED)
+	public UserView createUser(final UserView userView) {
+		return saveOrUpdateUser(userView);
+	}
 
-  @Override
-  @Transactional(propagation= Propagation.REQUIRED)
-  public UserView updateUser(final UserView userView) throws RecordNotFoundException {
-    return saveOrUpdateUser(userView);
-  }
+	@Override
+	@Transactional(propagation = Propagation.REQUIRED)
+	public UserView updateUser(final UserView userView) throws RecordNotFoundException {
+		return saveOrUpdateUser(userView);
+	}
 
-  @Override
-  public void deleteUserById(Long id) throws RecordNotFoundException {
-    userRepository.deleteById(id);
-  }
+	@Override
+	public void deleteUserById(Long id) throws RecordNotFoundException {
+		userRepository.deleteById(id);
+	}
 
-  private UserView prepareUserView(final User user) {
-    UserView userView = new UserView();
-    copyProperties(user, userView);
-    return userView;
-  }
+	private UserView prepareUserView(final User user) {
+		UserView userView = prepareUserViewFromUser(user);
+		return userView;
+	}
 
-  private UserView saveOrUpdateUser(final UserView userView) {
-    User user = new User();
-    copyProperties(userView, user);
+	private UserView saveOrUpdateUser(final UserView userView) {
+		User user = prepareUserFromUserView(userView);
+		User savedUser = userRepository.save(user);
+		UserView uv = prepareUserViewFromUser(savedUser);
+		return uv;
+	}
 
-    User savedUser = userRepository.save(user);
+	private List<UserView> prepareUserViewsFromUsers(final List<User> users) {
+		List<UserView> userViews = new ArrayList<>(users.size());
 
-    UserView uv = new UserView();
-    copyProperties(savedUser, uv);
-    return uv;
-  }
+		users.stream().forEach(u -> {
+			UserView userView = prepareUserViewFromUser(u);
+			userViews.add(userView);
+		});
+
+		return userViews;
+
+	}
+
+	private UserView prepareUserViewFromUser(final User user) {
+
+		UserView userView = new UserView(user.getId(), user.getLoginId(), user.getFirstName(), user.getLastName(),
+				user.getEmailId(), user.isActive());
+
+		List<Role> roles = user.getRoles();
+		List<RoleView> roleViews = new ArrayList<>(roles.size());
+
+		roles.stream().forEach(r -> {
+			RoleView roleView = new RoleView(r.getId(), r.getName(), r.getDescription(), r.isActive());
+			roleViews.add(roleView);
+		});
+
+		userView.setRoles(roleViews);
+
+		return userView;
+	}
+
+	private User prepareUserFromUserView(final UserView uv) {
+
+		User user = new User(uv.getId(), uv.getLoginId(), uv.getFirstName(), uv.getLastName(), uv.getEmailId(),
+				uv.isActive());
+
+		List<RoleView> roleViews = uv.getRoles();
+		List<Role> roles = new ArrayList<>(roleViews.size());
+
+		roleViews.stream().forEach(r -> {
+			Role role = new Role(r.getId(), r.getName(), r.getDescription(), r.isActive());
+			roles.add(role);
+		});
+
+		user.setRoles(roles);
+
+		return user;
+	}
 }
